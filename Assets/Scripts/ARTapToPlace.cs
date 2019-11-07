0a8c827952deb54b0d687c1af68fbcb7b6922ab7
zhangzifeng <zhangzifeng@DOMAIN.SENSETIME.COM>
2019-11-07 17:02:45 +0800

    AR Demos

---
+++ Assets/Scripts/ARTapToPlace.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using UnityEngine.UI;
+using System;
+
+public class ARTapToPlace : MonoBehaviour
+{
+    public GameObject placementIndicator;
+    public Text text;
+
+    private ARSessionOrigin _arOrigin;
+    private Pose _placementPose;
+    private bool _placementPoseIsValid = false;
+    private ARRaycastManager _arRay;
+
+    private void Start()
+    {
+        //_arOrigin = GameObject.Find("AR Session Origin").GetComponent<ARSessionOrigin>();
+        //_arRay = GameObject.Find("AR Session Origin").GetComponent<ARRaycastManager>();
+
+        _arOrigin = FindObjectOfType<ARSessionOrigin>();
+        _arRay = FindObjectOfType<ARRaycastManager>();
+    }
+
+    private void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    /// <summary>
+    /// update the pose of the placement per frame
+    /// </summary>
+    private void UpdatePlacementPose()
+    {
+        //viewport is from (0,0) at the bottom left to (1,1) at the top right. So the center is (0.5, 0.5)
+        if(Camera.current != null)
+        {
+            Vector3 screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+
+            //the list to store the hits that happen in the current frame
+            List<ARRaycastHit> hitsResult = new List<ARRaycastHit>();
+
+            //ray casted from the center of the screen & return the hits to the hitsResultList
+            _arRay.Raycast(screenCenter, hitsResult, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+            //if there is any hit, perform update
+            _placementPoseIsValid = hitsResult.Count > 0;
+            if (_placementPoseIsValid)
+            {
+                text.text = "true";
+                _placementPose = hitsResult[0].pose;
+                _placementPose.position.y += 0.05f;
+
+                //let the indicator point to camera's forward direction
+                Vector3 cameraForward = Camera.current.transform.forward;
+                Vector3 normalizedCameraForward = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+                _placementPose.rotation = Quaternion.LookRotation(normalizedCameraForward);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Update the indicator (aka the visual effect) to the new pose per frame
+    /// </summary>
+    private void UpdatePlacementIndicator()
+    {
+        if (_placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(_placementPose.position, _placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/DrawLine.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class DrawLine : MonoBehaviour
+{
+    public GameObject linePrefab;
+    public ARSessionOrigin aRSessionOrigin;
+    public GameObject lineAnchor;
+
+    private GameObject _currentLine;
+    private LineRenderer _lineRenderer;
+    private List<Vector3> _drawPositions = new List<Vector3>();
+    //private Vector3 _viewPortCenter = new Vector3(0.5f, 0.5f);
+    private Vector2 _screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+    private Vector3 _newPosition;
+
+    private ARReferencePointManager _pointManager;
+
+    public ARCameraManager arCameraManager;
+    public ARRaycastManager arRaycastManager;
+
+    private List<ARRaycastHit> _hit = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _pointManager = aRSessionOrigin.GetComponent<ARReferencePointManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch Began");
+                CreateLine();
+    }
+
+            if(touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved)
+            {
+
+                //_screenCenter = Camera.current.ViewportToScreenPoint(_viewPortCenter);
+                //_screenCenter = Camera.main.ViewportToScreenPoint(_viewPortCenter);
+                _newPosition = Camera.main.ScreenPointToRay(_screenCenter).direction;
+                _newPosition.z += 0.01f;
+                _newPosition += Camera.main.ScreenToWorldPoint(_screenCenter);
+
+
+                Debug.Log("newPosition" + _newPosition);
+
+                UpdateLine(_newPosition);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Touch Began");
+            CreateLine();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+
+        }
+    }
+
+    void CreateLine()
+    {
+        _drawPositions.Clear();
+        _lineRenderer = null;
+
+        _currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+        _lineRenderer = _currentLine.GetComponent<LineRenderer>();
+
+        //_screenCenter = Camera.main.ViewportToScreenPoint(_viewPortCenter);
+        Vector3 startPosition = Camera.main.ScreenPointToRay(_screenCenter).direction;
+        startPosition.z += 0.01f;
+        startPosition += Camera.main.ScreenToWorldPoint(_screenCenter);
+
+        Vector3 startRot = Camera.main.transform.rotation.eulerAngles;
+        Quaternion startRotation = Quaternion.Euler(startRot);
+
+        Pose p = new Pose();
+        p.rotation = startRotation;
+        p.position = startPosition;
+        _pointManager.AddReferencePoint(p);
+
+        lineAnchor.transform.position = startPosition;
+        lineAnchor.transform.rotation = startRotation;
+        _currentLine.transform.parent = lineAnchor.transform;
+
+        _drawPositions.Add(startPosition);
+        _drawPositions.Add(startPosition);
+
+        _lineRenderer.SetPosition(0, _drawPositions[0]);
+        _lineRenderer.SetPosition(1, _drawPositions[1]);
+    }
+
+    void UpdateLine(Vector3 position)
+    {
+        _drawPositions.Add(position);
+        _lineRenderer.positionCount++;
+        _lineRenderer.SetPosition(_lineRenderer.positionCount - 1, position);
+    }
+}
